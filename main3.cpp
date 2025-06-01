08cd6b1d2ffdfe0e4072d2216eeb6b28f7f51cc6
Qiu-Zi-Shan <1312113164@qq.com>
2025-06-01 18:41:11 +0800

    简单的游戏暂停功能

--- main3.cpp
+++ main3.cpp
@@ -1,10 +1,5 @@
 #include "Mental_Trajectory2.h"
 
-const string PlayerManager::PLAYER_DATA_FILE = "C:\\Cpl\\CPP_Pratical_Training\\player_data.txt";
-const string LeaderboardManager::CHALLENGE_LEADERBOARD_FILE = "C:\\Cpl\\CPP_Pratical_Training\\challenge_leaderboard.txt";
-const string LeaderboardManager::STAGE_LEADERBOARD_FILE = "C:\\Cpl\\CPP_Pratical_Training\\stage_leaderboard.txt";
-const string LeaderboardManager::LEVEL_TIME_FILE = "C:\\Cpl\\CPP_Pratical_Training\\level_time";
-
 int main(){
     GameRunner::runGame();
     return 0;
